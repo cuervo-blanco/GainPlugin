432060cecd7f09946bd486c15fed6e640dc90f16
cuervo-blanco <jaios.228@gmail.com>
2025-04-23 08:52:41 -0400

    Removed old project files

--- Source/GainPluginProcessor.cpp
+++
@@ -1,8 +0,0 @@
-#include "GainPluginProcessor.h"
-
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
-    return new GainPluginProcessor();
-}
-GainPluginProcessor::GainPluginProcessor() : GainPlugin() {}
-GainPluginProcessor::~GainPluginProcessor() {}
-
